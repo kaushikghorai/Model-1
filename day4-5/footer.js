35e6cc3179ae430c259974e2d65a0cc50a189193
Kaushik Ghorai <hello@kaushikghorai.in>
2026-05-15 14:17:09 +0530

    basic website is completed

---
+++ day4-5/footer.js
@@ -0,0 +1,1 @@
+window.document.getElementById("footer").innerHTML = "<div>&copy; Kaushik ghorai</div>";
---
+++ day4-5/header.js
@@ -0,0 +1,4 @@
+window.document.getElementById("header").innerHTML = "<div id='websiteName'></div><div id='navigations'></div><div id='account'></div>";
+window.document.getElementById("websiteName").innerHTML = "<h1>WEBNAME</h1>";
+window.document.getElementById("navigations").innerHTML = "<nav><a href='home.html'>Home</a><a href='category.html'>Category</a><a href='products.html'>Products</a><a href='cart.html'>Cart</a><a href='about.html'>About us</a><a href='contact.html'>Contact</a></nav>";
+window.document.getElementById("account").innerHTML = "<div id='myProfile'>My Profile</div>";
