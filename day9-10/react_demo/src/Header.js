d6347708432eb3d6d80de986e86bf06a088162f9
Kaushik Ghorai <hello@kaushikghorai.in>
2026-05-25 10:31:06 +0530

    Nodejs and reactjs

---
+++ day9-10/react_demo/src/Header.js
@@ -0,0 +1,16 @@
+import './Header.css';
+import {Link} from 'react-router-dom';
+function Header(){
+	return(
+		<div class="myheader">
+			<ul>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/about">AboutUs</Link></li>
+				<li><Link to="/gallery">Gallery</Link></li>
+				<li><Link to="/signup">Sign Up></Link></li>
+			</ul>	
+		</div>
+	);
+}
+
+export default Header;
