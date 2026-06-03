96e56de0625abe091912d3aca7459ec53f689f3f
Kaushik Ghorai <hello@kaushikghorai.in>
2026-06-03 14:06:47 +0530

    error resolved

--- day9-14/dashboard/frontend/src/Header.js
+++ day9-14/dashboard/frontend/src/Header.js
@@ -1,15 +1,15 @@
 import './Header.css';
-import {Link} from 'react-router-dom';
-function Header(){
-	return(
+import { Link } from 'react-router-dom';
+function Header() {
+	return (
 		<div class="myheader">
 			<ul>
 				<li><Link to="/">Home</Link></li>
 				<li><Link to="/about">AboutUs</Link></li>
-				<li><Link to="/gallery">Gallery</Link></li>
-                <li><Link to="/products">Products</Link></li>
+				<li><a href="/website/home.html">Website</a></li>
+				<li><Link to="/products">Products</Link></li>
 				<li><Link to="/signup">Sign Up</Link></li>
-			</ul>	
+			</ul>
 		</div>
 	);
 }
